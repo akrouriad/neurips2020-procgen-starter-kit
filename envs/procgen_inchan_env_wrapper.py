7be61b1f94e92639dde0cbf75bd73e24bba75b1b
Riad <riad@robot-learning.de>
2020-09-02 15:46:22 +0200

    other wrappers

---
+++ envs/procgen_inchan_env_wrapper.py
@@ -0,0 +1,42 @@
+from gym import Wrapper
+from envs.procgen_env_wrapper import ProcgenEnvWrapper
+import numpy as np
+from ray.tune import registry
+
+
+class DiffInChanWrapper(Wrapper):
+    def __init__(self, env):
+        super().__init__(env)
+        self._lastobs = None
+        self._lastlastobs = None
+
+    def reset(self):
+        self._lastobs = self.env.reset()
+        self._lastlastobs = None
+        ret = self._lastobs.copy()
+        ret[:, :, 2] = (np.sum(ret, axis=2) + ret[:, :, 2]) / 4
+        ret[:, :, 0:1] = 0
+        return ret.astype(np.uint8)
+
+    def step(self, action):
+        obs, rew, done, info = self.env.step(action)
+        ret = obs.copy()
+        ret[:, :, 2] = (np.sum(ret, axis=2) + ret[:, :, 2]) / 4
+        fdiff = obs - self._lastobs
+        ret[:, :, 1] = (np.sum(fdiff, axis=2) + fdiff[:, :, 1]) / 4
+        if self._lastlastobs is not None:
+            sdiff = self._lastobs - self._lastlastobs
+            sdiff = fdiff - sdiff
+            ret[:, :, 0] = (np.sum(sdiff, axis=2) + sdiff[:, :, 0]) / 4
+        else:
+            ret[:, :, 0] = 0.
+        self._lastlastobs = self._lastobs
+        self._lastobs = obs
+        return ret.astype(np.uint8), rew, done, info
+
+
+# Register Env in Ray
+registry.register_env(
+    "procgen_inchan_env_wrapper",
+    lambda config: DiffInChanWrapper(ProcgenEnvWrapper(config))
+)
